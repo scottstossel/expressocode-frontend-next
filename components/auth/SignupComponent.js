e25e1ef6785d13aa4126b012ba19666641c02f34
Scott Stossel <stoss96@gmail.com>
2021-10-15 20:34:00 -0400

    auth connected for sign up

--- components/auth/SignupComponent.js
+++ components/auth/SignupComponent.js
@@ -1,25 +1,87 @@
+import { useState } from "react";
+import { signup } from "../../actions/auth";
+
 const SignupComponent = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    message: "",
+    showForm: true,
+  });
+
+  const { name, email, password, error, loading, message, showForm } = values;
+
   const handleSumbit = (e) => {
     e.preventDefault();
-    console.log('handle submit');
-  }
+    // console.table({ name, email, password, error, loading, message, showForm });
+    setValues({ ...values, loading: true, error: false });
+    const user = { name, email, password };
+    signup(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          loading: false,
+          message: data.message,
+          showForm: false
+        });
+      }
+    });
+  };
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  }
+  const handleChange = (name) => (e) => {
+    setValues({
+      ...values,
+      error: false,
+      [name]: e.target.value,
+    });
+  };
 
   const signupForm = () => {
     return (
       <form onSubmit={handleSumbit}>
         <div>
-          <input onChange={handleChange} type="text" className="form-control" placeholder="type your name" />
+          <input
+            value={name}
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+            placeholder="type your name"
+          />
+        </div>
+        <div>
+          <input
+            value={email}
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+            placeholder="type your email"
+          />
+        </div>
+        <div>
+          <input
+            value={password}
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+            placeholder="type your password"
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">Sign Up</button>
         </div>
       </form>
-    )
-  }
-  return (
-    {signupForm()}
-  )
-}
+    );
+  };
+  return <>{signupForm()}</>;
+};
 
 export default SignupComponent;
