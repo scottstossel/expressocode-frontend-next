0f626c517aeb0c025f641f43e1a43ebb0d5ff786
Scott Stossel <stoss96@gmail.com>
2021-10-17 23:52:50 -0400

    detected error in signup

--- components/auth/SignupComponent.js
+++ components/auth/SignupComponent.js
@@ -1,87 +1,99 @@
-import { useState } from "react";
-import { signup } from "../../actions/auth";
+import { useState } from 'react';
+import { signup } from '../../actions/auth';
 
 const SignupComponent = () => {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    password: "",
-    error: "",
-    loading: false,
-    message: "",
-    showForm: true,
-  });
+    const [values, setValues] = useState({
+        name: 'Ryan',
+        email: 'ryan@gmail.com',
+        password: 'rrrrrr',
+        error: '',
+        loading: false,
+        message: '',
+        showForm: true
+    });
 
-  const { name, email, password, error, loading, message, showForm } = values;
+    const { name, email, password, error, loading, message, showForm } = values;
 
-  const handleSumbit = (e) => {
-    e.preventDefault();
-    // console.table({ name, email, password, error, loading, message, showForm });
-    setValues({ ...values, loading: true, error: false });
-    const user = { name, email, password };
-    signup(user).then((data) => {
-      if (data.error) {
-        setValues({ ...values, error: data.error, loading: false });
-      } else {
-        setValues({
-          ...values,
-          name: "",
-          email: "",
-          password: "",
-          error: "",
-          loading: false,
-          message: data.message,
-          showForm: false
+    const handleSubmit = e => {
+        e.preventDefault();
+        // console.table({ name, email, password, error, loading, message, showForm });
+        setValues({ ...values, loading: true, error: false });
+        const user = { name, email, password };
+        console.log(data);
+        signup(user).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, loading: false });
+            } else {
+                setValues({
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
+                    error: '',
+                    loading: false,
+                    message: data.message,
+                    showForm: false
+                });
+            }
         });
-      }
-    });
-  };
+    };
 
-  const handleChange = (name) => (e) => {
-    setValues({
-      ...values,
-      error: false,
-      [name]: e.target.value,
-    });
-  };
+    const handleChange = name => e => {
+        setValues({ ...values, error: false, [name]: e.target.value });
+    };
 
-  const signupForm = () => {
+    const showLoading = () => (loading ? <div className="alert alert-info">Loading...</div> : '');
+    const showError = () => (error ? <div className="alert alert-danger">{error}</div> : '');
+    const showMessage = () => (message ? <div className="alert alert-info">{message}</div> : '');
+
+    const signupForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input
+                        value={name}
+                        onChange={handleChange('name')}
+                        type="text"
+                        className="form-control"
+                        placeholder="Type your name"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        value={email}
+                        onChange={handleChange('email')}
+                        type="email"
+                        className="form-control"
+                        placeholder="Type your email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        value={password}
+                        onChange={handleChange('password')}
+                        type="password"
+                        className="form-control"
+                        placeholder="Type your password"
+                    />
+                </div>
+
+                <div>
+                    <button className="btn btn-primary">Signup</button>
+                </div>
+            </form>
+        );
+    };
+
     return (
-      <form onSubmit={handleSumbit}>
-        <div>
-          <input
-            value={name}
-            onChange={handleChange("name")}
-            type="text"
-            className="form-control"
-            placeholder="type your name"
-          />
-        </div>
-        <div>
-          <input
-            value={email}
-            onChange={handleChange("email")}
-            type="email"
-            className="form-control"
-            placeholder="type your email"
-          />
-        </div>
-        <div>
-          <input
-            value={password}
-            onChange={handleChange("password")}
-            type="password"
-            className="form-control"
-            placeholder="type your password"
-          />
-        </div>
-        <div>
-          <button className="btn btn-primary">Sign Up</button>
-        </div>
-      </form>
+        <>
+            {showError()}
+            {showLoading()}
+            {showMessage()}
+            {showForm && signupForm()}
+        </>
     );
-  };
-  return <>{signupForm()}</>;
 };
 
 export default SignupComponent;
--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,18 @@
-import Layout from "../components/Layout";
-import Link from "next/link";
-import SignupComponent from "../components/auth/SignupComponent";
+import Layout from '../components/Layout';
+import SignupComponent from '../components/auth/SignupComponent';
+import Link from 'next/link';
 
 const Signup = () => {
-  return (
-    <Layout>
-      <h2>Signup Page</h2>
-      <SignupComponent />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <h2 className="text-center pt-4 pb-4">Signup</h2>
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <SignupComponent />
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default Signup;
