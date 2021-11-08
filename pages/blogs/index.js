d9ef08fa1b7cea165242a49765e3e8997b793bc0
Scott Stossel <stoss96@gmail.com>
2021-11-07 19:30:56 -0500

    render blogs on blogs page

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,11 +1,52 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Layout from '../../components/Layout';
-import { useState } from 'react';
-import { listBlogsWithCategoriesAndTags } from '../../actions/blogActions';
-import { API } from '../../config';
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import { useState } from "react";
+import { listBlogsWithCategoriesAndTags } from "../../actions/blogActions";
+import { API } from "../../config";
+import renderHTML from 'react-render-html';
+import moment from "moment";
 
-const Blogs = () => {
+const Blogs = ({ blogs, categories, tags, size }) => {
+  const showAllBlogs = () => {
+    return blogs.map((blog, i) => (
+      <>
+        <article key={i} className="lead pb-4">
+          <div className="lead">
+            <header>
+              <Link href={`/blogs/${blog.slug}`}>
+                <a>
+                  <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
+                </a>
+              </Link>
+            </header>
+            <section>
+              <p className="mark ml-1 pt-2 pb2">
+                Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
+              </p>
+            </section>
+            <section>
+              <p>Blog categories and tags</p>
+            </section>
+            <div className="row">
+              <div className="col-md-4">image</div>
+              <div className="col-md-8">
+                <section>
+                <div className="pb-3">
+                  {renderHTML(blog.excerpt)}
+                </div>
+                  <Link href={`/blogs/${blog.slug}`}>
+                    <a className="btn btn-primary pt-2">Read more</a>
+                  </Link>
+                </section>
+              </div>
+            </div>
+          </div>
+          <hr />
+        </article>
+      </>
+    ));
+  };
   return (
     <>
       <Layout>
@@ -13,7 +54,9 @@ const Blogs = () => {
           <div className="container-fluid">
             <header>
               <div className="col-md-12 pt-3">
-                <h1 className="display-4 font-weight-bold text-center">Programming blogs and tutorials</h1>
+                <h1 className="display-4 font-weight-bold text-center">
+                  Programming blogs and tutorials
+                </h1>
               </div>
               <section>
                 <p>show categories and tags</p>
@@ -21,12 +64,27 @@ const Blogs = () => {
             </header>
           </div>
           <div className="container-fluid">
-            <div className="col-md-12">show all blogs</div>
+            <div className="col-md-12">{showAllBlogs()}</div>
           </div>
         </main>
       </Layout>
     </>
-  )
-}
+  );
+};
+
+Blogs.getInitialProps = () => {
+  return listBlogsWithCategoriesAndTags().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      return {
+        blogs: data.blogs,
+        categories: data.categories,
+        tags: data.tags,
+        size: data.size,
+      };
+    }
+  });
+};
 
 export default Blogs; //getInitialProps
