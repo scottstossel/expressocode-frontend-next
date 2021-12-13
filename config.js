e75abeb26dc7e6d834705e1fddeadb16f5b4b65f
Scott Stossel <stoss96@gmail.com>
2021-12-12 23:53:25 -0500

    open graphs for header content

--- config.js
+++ config.js
@@ -4,3 +4,7 @@ const {publicRuntimeConfig} = getConfig();
 export const API = publicRuntimeConfig.PRODUCTION ? 'https://expressocode.com' : 'http://localhost:8000/api';
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
+
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.DOMAIN_DEVELOPMENT
+  : publicRuntimeConfig.DOMAIN_PRODUCTION;
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -3,12 +3,37 @@ import Link from "next/link";
 import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blogActions";
-import { API } from "../../config";
+// import { API } from "../../config";
 import renderHTML from "react-render-html";
 import moment from "moment";
 import Card from "../../components/blog/Card";
+import { API, DOMAIN, APP_NAME } from "../../config";
+import { withRouter } from "next/router";
 
-const Blogs = ({ blogs, categories, tags, size }) => {
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+  const head = () => (
+    <Head>
+      <title>Programming blogs | {APP_NAME}</title>
+      <meta
+        name="description"
+        content="Programming blogs and tutorials on react node next vue php laravel and web development"
+      />
+      <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+      <meta
+        property="og:title"
+        content={`Your digital hangout for all things programming related | ${APP_NAME}`}
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+      
+      <meta property="og:image" content={`${APP_NAME}`} />
+      <meta property="og:image:secure_url" content={`${APP_NAME}`} />
+      <meta property="og:image:type" content={`${APP_NAME}`} />
+      <meta property="fb:app_id" content={`${APP_NAME}`} />
+    </Head>
+  );
+
   const showAllCategories = () => {
     return categories.map((c, i) => (
       <Link href={`/categories/${c.slug}`} key={i}>
@@ -21,7 +46,10 @@ const Blogs = ({ blogs, categories, tags, size }) => {
   const showAllTags = () => {
     return tags.map((t, i) => (
       <Link href={`/tags/${t.slug}`} key={i}>
-        <a className="btn btn-outline-primary mt-3" style={{ marginRight: "5px" }}>
+        <a
+          className="btn btn-outline-primary mt-3"
+          style={{ marginRight: "5px" }}
+        >
           {t.name}
         </a>
       </Link>
